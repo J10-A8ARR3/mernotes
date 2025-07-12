b4cccb4d0fd8787b020db6a6d971e21f5946eafc
Jeo Abarre <136715818+J10-A8ARR3@users.noreply.github.com>
2025-07-12 21:26:28 +0800

    mongoDB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://jcabarre05:4yWpNyDbnVr6wsKw@cluster0.o0tpamm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MONGODB CONNECTED SUCCESFULLY!");
   } catch (error) {
     console.error("Error connecting to MONGODB");
+    process.exit(1);
   }
 };
