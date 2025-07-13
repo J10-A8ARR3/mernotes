f0d33a15853dbcd8f2eecb7c778c932581b68bb4
Jeo Abarre <136715818+J10-A8ARR3@users.noreply.github.com>
2025-07-13 23:16:08 +0800

    detailing

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,12 @@ import express from "express";
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import rateLimiter from "./middleware/rateLimiter.js"
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-connectDB();
 
 //middleware
 app.use(express.json());
@@ -22,6 +20,8 @@ app.use(rateLimiter);
 
 app.use("/api/notes", noteRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server started on PORT:", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  })
 });
