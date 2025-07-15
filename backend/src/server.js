3e0815916d695ccac6db5e493eed8b5994648d21
Jeo Abarre <136715818+J10-A8ARR3@users.noreply.github.com>
2025-07-15 15:24:58 +0800

    cors and fetching datas from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -12,6 +14,11 @@ const PORT = process.env.PORT || 5001;
 //middleware
 app.use(express.json());
 app.use(rateLimiter);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 // app.use((req, res, next) => {
 //   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
@@ -23,5 +30,5 @@ app.use("/api/notes", noteRoutes);
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
-  })
+  });
 });
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
 
 const HomePage = () => {
   const [isRateLimited, setRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+      } catch (error) {
+        console.log("Error Fetching Notes")
+      }
+    };
 
+    fetchNotes();
+  }, []);
 
   return (
     <div className="min-h-screen">
